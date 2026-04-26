e105cce9816811bfebddb263157f837c84d0015d
RomanGulevatiy <gulevatiy.roman@gmail.com>
2026-04-26 20:51:58 +0300

    Initial commit

---
+++ src/main/java/com/duikt/malachite/MalachiteApplication.java
@@ -0,0 +1,13 @@
+package com.duikt.malachite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalachiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalachiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/duikt/malachite/MalachiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duikt.malachite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalachiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
